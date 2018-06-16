fb02c8fd59bc9232691817509e5f3d11f2b06ba4
Zoldur <zoldur99@gmail.com>
2018-06-16 03:14:39 -0700

    Get IP

--- alps_install.sh
+++ alps_install.sh
@@ -12,7 +12,7 @@ COIN_NAME='Alps'
 COIN_PORT=9666
 RPC_PORT=9667
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -142,7 +142,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
